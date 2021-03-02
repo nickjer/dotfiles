26e71e624eddee4434105a76e86c1981ef6896ff
Jeremy Nicklas <jeremywnicklas@gmail.com>
2021-03-01 20:49:59 -0500

    changes

--- bootstrap_script.sh
+++ bootstrap_script.sh
@@ -330,5 +330,14 @@ echo "Bootstrapping vim..."
 
 # Download/install coc.nvim extensions
 "${HOME}/bin/nvim" \
-  '+CocInstall -sync coc-explorer coc-fish coc-git coc-json coc-pairs coc-rls coc-sh coc-snippets coc-solargraph' \
+  "+CocInstall -sync \
+    coc-explorer \
+    coc-fish \
+    coc-git \
+    coc-json \
+    coc-pairs \
+    coc-rls \
+    coc-sh \
+    coc-snippets \
+    coc-solargraph" \
   '+qall'
