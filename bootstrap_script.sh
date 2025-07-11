66fe3b5d25e2d650c3feebf4aeefbd7249183ba1
Jeremy Nicklas <jeremywnicklas@gmail.com>
2025-07-11 19:34:20 -0400

    move bin dir

--- bootstrap_script.sh
+++ bootstrap_script.sh
@@ -2,75 +2,75 @@
 
 set -eo pipefail
 
-mkdir -p ~/bin
+mkdir -p ~/.local/bin
 
 # Install fish if missing
-if ! command -v fish &> /dev/null ; then
-  sudo apt-add-repository ppa:fish-shell/release-3
+if ! command -v fish &>/dev/null; then
+  sudo apt-add-repository ppa:fish-shell/release-4
   sudo apt-get update
   sudo apt-get install fish
 fi
 
 # Install make if missing
-if ! command -v make &> /dev/null ; then
+if ! command -v make &>/dev/null; then
   sudo apt install -y make
 fi
 
 # Install curl if missing
-if ! command -v curl &> /dev/null ; then
+if ! command -v curl &>/dev/null; then
   sudo apt install -y curl
 fi
 
 # Install tar if missing
-if ! command -v tar &> /dev/null ; then
+if ! command -v tar &>/dev/null; then
   sudo apt install -y tar
 fi
 
 # Install openssl if missing
-if ! command -v openssl &> /dev/null ; then
+if ! command -v openssl &>/dev/null; then
   sudo apt install -y openssl
 fi
 
 # Install bzip2 if missing
-if ! command -v bzip2 &> /dev/null ; then
+if ! command -v bzip2 &>/dev/null; then
   sudo apt install -y bzip2
 fi
 
 # Install unzip if missing
-if ! command -v unzip &> /dev/null ; then
+if ! command -v unzip &>/dev/null; then
   sudo apt install -y unzip
 fi
 
 # Install patch if missing
-if ! command -v patch &> /dev/null ; then
+if ! command -v patch &>/dev/null; then
   sudo apt install -y patch
 fi
 
 # Install gcc if missing
-if ! command -v gcc &> /dev/null ; then
+if ! command -v gcc &>/dev/null; then
   sudo apt install build-essential
 fi
 
 # Install xsel if missing
-if ! command -v xsel &> /dev/null ; then
+if ! command -v xsel &>/dev/null; then
   sudo apt install -y xsel
 fi
 
 # Install libfuse2 if missing
-if [[ ! -f "/usr/lib/x86_64-linux-gnu/libfuse.so.2" ]] ; then
+if [[ ! -f "/usr/lib/x86_64-linux-gnu/libfuse.so.2" ]]; then
   sudo apt install -y libfuse2
 fi
 
 # Copy over dotfiles
 (
   cd src
-  for FILE in $(find . -type f) ; do
+  for FILE in $(find . -type f); do
     COPY_PATH="$(realpath -m -s "${HOME}/${FILE}")"
     mkdir -p "$(dirname "${COPY_PATH}")"
 
     ORIG_PATH="$(realpath "${FILE}")"
-    if [[ ! -L "${COPY_PATH}" ]] ; then
-      if [[ -e "${COPY_PATH}" ]] ; then
+    if [[ ! -L "${COPY_PATH}" ]]; then
+      if [[ -e "${COPY_PATH}" ]]; then
         BACKUP_PATH="${COPY_PATH}.bak"
         echo "Backing up ${COPY_PATH} to ${BACKUP_PATH}"
         mv "${COPY_PATH}" "${BACKUP_PATH}"
@@ -85,170 +85,147 @@ fi
 function installTools() {
   # Download/install neovim
   echo "Downloading and installing 'neovim'"
-  local url="$(~/bin/ghlast neovim neovim --output assets | grep 'x86_64\.appimage$')"
+  local url="$(~/.local/bin/ghlast neovim neovim --output assets | grep 'x86_64\.appimage$')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" -s -S -f -o nvim && \
-      chmod 755 nvim && \
-      mv nvim ~/bin
+    cd "${tmp}" &&
+      curl -L "${url}" -s -S -f -o nvim &&
+      chmod 755 nvim &&
+      mv nvim ~/.local/bin
   )
-
-  # Download/install uctags
-  echo "Downloading and installing 'uctags'"
-  local url="$(~/bin/ghlast universal-ctags ctags-nightly-build --output assets | grep 'linux-x86_64\.release\.tar\.gz$')"
-  local tmp="$(mktemp -d)"
-  (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz --strip-components=1 && \
-      mv bin/* ~/bin/.
-  )
-  rm -fr "${tmp}"
 
   # Download/install starship
   echo "Downloading and installing 'starship'"
-  local url="$(~/bin/ghlast starship starship --output assets | grep 'x86.*musl.*gz$')"
+  local url="$(~/.local/bin/ghlast starship starship --output assets | grep 'x86.*musl.*gz$')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz && \
-      mv starship ~/bin
+    cd "${tmp}" &&
+      curl -L "${url}" | tar xz &&
+      mv starship ~/.local/bin
   )
   rm -fr "${tmp}"
 
   # Download/install yayo
   echo "Downloading and installing 'yayo'"
-  local url="$(~/bin/ghlast nickjer yayo --output assets | grep 'x86.*musl.*gz$')"
+  local url="$(~/.local/bin/ghlast nickjer yayo --output assets | grep 'x86.*musl.*gz$')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz && \
-      mv yayo ~/bin
+    cd "${tmp}" &&
+      curl -L "${url}" | tar xz &&
+      mv yayo ~/.local/bin
   )
   rm -fr "${tmp}"
 
   # Download/install fltn
   echo "Downloading and installing 'fltn'"
-  local url="$(~/bin/ghlast nickjer fltn --output assets | grep 'x86.*musl.*gz$')"
+  local url="$(~/.local/bin/ghlast nickjer fltn --output assets | grep 'x86.*musl.*gz$')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz && \
-      mv fltn ~/bin
+    cd "${tmp}" &&
+      curl -L "${url}" | tar xz &&
+      mv fltn ~/.local/bin
   )
   rm -fr "${tmp}"
 
   # Download/install ripgrep
   echo "Downloading and installing 'ripgrep'"
-  local url="$(~/bin/ghlast BurntSushi ripgrep --output assets | grep 'x86.*musl.*gz$')"
+  local url="$(~/.local/bin/ghlast BurntSushi ripgrep --output assets | grep 'x86.*musl.*gz$')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz --strip-components=1 && \
-      mv rg ~/bin
+    cd "${tmp}" &&
+      curl -L "${url}" | tar xz --strip-components=1 &&
+      mv rg ~/.local/bin
   )
   rm -fr "${tmp}"
 
   # Download/install delta
   echo "Downloading and installing 'delta'"
-  local url="$(~/bin/ghlast dandavison delta --output assets | grep 'x86.*musl.*gz$')"
+  local url="$(~/.local/bin/ghlast dandavison delta --output assets | grep 'x86.*musl.*gz$')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz --strip-components=1 && \
-      mv delta ~/bin
+    cd "${tmp}" &&
+      curl -L "${url}" | tar xz --strip-components=1 &&
+      mv delta ~/.local/bin
   )
   rm -fr "${tmp}"
 
   # Download/install xh
   echo "Downloading and installing 'xh'"
-  local url="$(~/bin/ghlast ducaale xh --output assets | grep 'x86.*musl.*gz$')"
+  local url="$(~/.local/bin/ghlast ducaale xh --output assets | grep 'x86.*musl.*gz$')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz --strip-components=1 && \
-      mv xh ~/bin && \
-      rm -f ~/bin/xhs && \
-      ln -s ~/bin/xh ~/bin/xhs
+    cd "${tmp}" &&
+      curl -L "${url}" | tar xz --strip-components=1 &&
+      mv xh ~/.local/bin &&
+      rm -f ~/.local/bin/xhs &&
+      ln -s ~/.local/bin/xh ~/.local/bin/xhs
   )
   rm -fr "${tmp}"
 
   # Download/install fzf
   echo "Downloading and installing 'fzf'"
-  local url="$(~/bin/ghlast junegunn fzf --output assets | grep 'linux_amd64')"
+  local url="$(~/.local/bin/ghlast junegunn fzf --output assets | grep 'linux_amd64')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz && \
-      mv fzf ~/bin
+    cd "${tmp}" &&
+      curl -L "${url}" | tar xz &&
+      mv fzf ~/.local/bin
   )
   rm -fr "${tmp}"
 
   # Download/install fd
   echo "Downloading and installing 'fd'"
-  local url="$(~/bin/ghlast sharkdp fd --output assets | grep 'x86.*gnu.*gz$')"
+  local url="$(~/.local/bin/ghlast sharkdp fd --output assets | grep 'x86.*gnu.*gz$')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz --strip-components=1 && \
-      mv fd ~/bin
+    cd "${tmp}" &&
+      curl -L "${url}" | tar xz --strip-components=1 &&
+      mv fd ~/.local/bin
   )
   rm -fr "${tmp}"
 
   # Download/install bat
   echo "Downloading and installing 'bat'"
-  local url="$(~/bin/ghlast sharkdp bat --output assets | grep 'x86.*gnu.*gz$')"
+  local url="$(~/.local/bin/ghlast sharkdp bat --output assets | grep 'x86.*gnu.*gz$')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz --strip-components=1 && \
-      mv bat ~/bin
+    cd "${tmp}" &&
+      curl -L "${url}" | tar xz --strip-components=1 &&
+      mv bat ~/.local/bin
   )
   rm -fr "${tmp}"
 
   # Download/install tldr
   echo "Downloading and installing 'tldr'"
-  local url="$(~/bin/ghlast dbrgn tealdeer --output assets | grep 'x86.*musl$')"
-  curl -L "${url}" -o ~/bin/tldr && \
-    chmod 755 ~/bin/tldr
-  ~/bin/tldr --update || true
-  local url="$(~/bin/ghlast dbrgn tealdeer --output assets | grep 'completions_fish')"
-  mkdir -p ~/.config/fish/completions && \
+  local url="$(~/.local/bin/ghlast dbrgn tealdeer --output assets | grep 'x86.*musl$')"
+  curl -L "${url}" -o ~/.local/bin/tldr &&
+    chmod 755 ~/.local/bin/tldr
+  ~/.local/bin/tldr --update || true
+  local url="$(~/.local/bin/ghlast dbrgn tealdeer --output assets | grep 'completions_fish')"
+  mkdir -p ~/.config/fish/completions &&
     curl -L "${url}" -o ~/.config/fish/completions/tldr.fish
-
-  # Download/install jira
-  echo "Downloading and installing 'jira'"
-  local url="$(~/bin/ghlast go-jira jira --output assets | grep 'linux-amd64$')"
-  curl -L "${url}" -o ~/bin/jira && \
-    chmod 755 ~/bin/jira
 
   # Download/install sd
   echo "Downloading and installing 'sd'"
-  local url="$(~/bin/ghlast chmln sd --output assets | grep 'x86.*musl')"
+  local url="$(~/.local/bin/ghlast chmln sd --output assets | grep 'x86.*musl')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz --strip-components=1 && \
-      mv sd ~/bin
+    cd "${tmp}" &&
+      curl -L "${url}" | tar xz --strip-components=1 &&
+      mv sd ~/.local/bin
   )
   rm -fr "${tmp}"
 
   # Download/install lsd
   echo "Downloading and installing 'lsd'"
-  local url="$(~/bin/ghlast lsd-rs lsd --output assets | grep 'x86.*gnu.*gz$')"
+  local url="$(~/.local/bin/ghlast lsd-rs lsd --output assets | grep 'x86.*gnu.*gz$')"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl -L "${url}" | tar xz --strip-components=1 && \
-      mv lsd ~/bin
+    cd "${tmp}" &&
+      curl -L "${url}" | tar xz --strip-components=1 &&
+      mv lsd ~/.local/bin
   )
   rm -fr "${tmp}"
-
-  # Download/install treesitter
-  echo "Downloading and installing 'treesitter'"
-  local url="$(~/bin/ghlast tree-sitter tree-sitter --output assets | grep 'linux-x64')"
-  curl -L "${url}" | gunzip > ~/bin/tree-sitter && \
-    chmod 755 ~/bin/tree-sitter
 }
 
 function installGhlast() {
@@ -258,20 +235,20 @@ function installGhlast() {
   local url="$(curl --silent "${api}" | grep 'browser_download_url' | grep 'unknown-linux-musl' | cut --delimiter '"' --fields 4)"
   local tmp="$(mktemp -d)"
   (
-    cd "${tmp}" && \
-      curl --location "${url}" | tar --extract --gzip && \
-      mv ghlast ~/bin
+    cd "${tmp}" &&
+      curl --location "${url}" | tar --extract --gzip &&
+      mv ghlast ~/.local/bin
   )
   rm --force --recursive "${tmp}"
 }
 
 function installMise() {
   echo "Installing mise..."
-  local url="$(~/bin/ghlast jdx mise --output assets | grep 'linux-x64$')"
-  curl -L "${url}" -o ~/bin/mise && \
-    chmod 755 ~/bin/mise
+  local url="$(~/.local/bin/ghlast jdx mise --output assets | grep 'linux-x64$')"
+  curl -L "${url}" -o ~/.local/bin/mise &&
+    chmod 755 ~/.local/bin/mise
 
-  source <(~/bin/mise activate bash)
+  source <(~/.local/bin/mise activate bash)
 }
 
 function installRuby() {
@@ -299,17 +276,17 @@ installGhlast
 installMise
 
 # Install ruby if missing
-if ! command -v ruby &> /dev/null ; then
+if ! command -v ruby &>/dev/null; then
   installRuby
 fi
 
 # Install node if missing
-if ! command -v node &> /dev/null ; then
+if ! command -v node &>/dev/null; then
   installNode
 fi
 
 # Install yarn if missing
-if ! command -v yarn &> /dev/null ; then
+if ! command -v yarn &>/dev/null; then
   installYarn
 fi
 
@@ -324,4 +301,4 @@ fish -c '
 
 # Install vim plugins
 echo "Bootstrapping vim..."
-"${HOME}/bin/nvim" --headless "+Lazy! sync" +qa
+"${HOME}/.local/bin/nvim" --headless "+Lazy! sync" +qa
--- src/.config/fish/config.fish
+++ src/.config/fish/config.fish
@@ -1,9 +1,9 @@
 # set PATH so it includes user's private bin if it exists
-fish_add_path ~/bin
+fish_add_path ~/.local/bin
 
 # mise
-if command -v mise &> /dev/null
-  mise activate fish | source
+if command -v mise &>/dev/null
+    mise activate fish | source
 end
 
 # yarn
@@ -12,9 +12,12 @@ fish_add_path ~/.yarn/bin
 # cargo
 fish_add_path ~/.cargo/bin
 
+# kitty
+fish_add_path ~/.local/kitty.app/bin
+
 # yayo
-if command -v yayo &> /dev/null
-  yayo completion fish | source
+if command -v yayo &>/dev/null
+    yayo completion fish | source
 end
 
 # Fix issue with QT applications and scaling
@@ -34,7 +37,7 @@ set -x BP_APP_ROOT $HOME/Development
 
 # Set GitHub token
 if test -z "$PKG_GITHUB_COM_TOKEN"
-  set -x PKG_GITHUB_COM_TOKEN ""
+    set -x PKG_GITHUB_COM_TOKEN ""
 end
 
 # Vim keybindings
@@ -45,37 +48,37 @@ starship init fish | source
 
 # Define abbreviations
 if status --is-interactive
-  abbr --add --global vim 'nvim'
-  abbr --add --global open 'xdg-open &> /dev/null'
+    abbr --add --global vim nvim
+    abbr --add --global open 'xdg-open &> /dev/null'
 
-  # bundle
-  abbr --add --global bi 'bundle install'
-  abbr --add --global be 'bundle exec'
+    # bundle
+    abbr --add --global bi 'bundle install'
+    abbr --add --global be 'bundle exec'
 
-  # git
-  abbr --add --global ga 'git add'
-  abbr --add --global gb 'git branch'
-  abbr --add --global gc 'git commit --verbose'
-  abbr --add --global gcm 'git commit --verbose --message'
-  abbr --add --global gco 'git checkout'
-  abbr --add --global gcb 'git checkout -b'
-  abbr --add --global gd 'git diff'
-  abbr --add --global gl 'git pull'
-  abbr --add --global gp 'git push'
-  abbr --add --global gs 'git status'
-  abbr --add --global gg 'git log --graph --pretty=format:'\''%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset'\'' --abbrev-commit --date=relative'
+    # git
+    abbr --add --global ga 'git add'
+    abbr --add --global gb 'git branch'
+    abbr --add --global gc 'git commit --verbose'
+    abbr --add --global gcm 'git commit --verbose --message'
+    abbr --add --global gco 'git checkout'
+    abbr --add --global gcb 'git checkout -b'
+    abbr --add --global gd 'git diff'
+    abbr --add --global gl 'git pull'
+    abbr --add --global gp 'git push'
+    abbr --add --global gs 'git status'
+    abbr --add --global gg 'git log --graph --pretty=format:'\''%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset'\'' --abbrev-commit --date=relative'
 
-  # docker-compose
-  abbr --add --global dco 'docker compose'
+    # docker-compose
+    abbr --add --global dco 'docker compose'
 
-  # lsd
-  abbr --add --global ls 'lsd'
-  abbr --add --global ll 'lsd --long'
-  abbr --add --global la 'lsd --all --long'
-  abbr --add --global tree 'lsd --tree'
+    # lsd
+    abbr --add --global ls lsd
+    abbr --add --global ll 'lsd --long'
+    abbr --add --global la 'lsd --all --long'
+    abbr --add --global tree 'lsd --tree'
 end
 
 # Set title of terminal
 function fish_title
-  basename (pwd)
+    basename (pwd)
 end
