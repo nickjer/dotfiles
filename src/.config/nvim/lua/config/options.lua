4980238290ed4c3b4c48a816c625321948b7d7d0
Jeremy Nicklas <jeremywnicklas@gmail.com>
2025-08-23 11:47:27 -0400

    add endwise and fix ai

--- src/.config/nvim/lua/config/options.lua
+++ src/.config/nvim/lua/config/options.lua
@@ -6,3 +6,5 @@ local opt = vim.opt
 
 opt.colorcolumn = "80" -- Colored column
 opt.relativenumber = false -- Relative line numbers
+
+vim.g.ai_cmp = false -- Disable AI completion in blink auto-complete menu
